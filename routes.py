4988f08b2e5d7106e0578891dd9c6679de0594ae
Julie Lavoie <almosteverywhere@gmail.com>
2013-08-19 18:41:59 -0400

    Added wtforms and bootstrap forms

--- routes.py
+++ routes.py
@@ -1,16 +1,48 @@
-from flask import Flask, render_template, flash, request, redirect, session, url_for, g
+# print dir()
+# import sys
+# print sys.modules
+from flask import Flask, render_template, flash, jsonify, request, redirect, session, url_for, g
 from functools import wraps
 import requests, json
+
+from flask_wtf import Form
+from wtforms import TextField, HiddenField, validators
+
+from wtforms.validators import DataRequired
+from flask.ext.bootstrap import Bootstrap
+# from flask.ext.wtf import Required
+
+##
+# from flask_wtf import Form
+# from wtforms.ext.sqlalchemy.orm import model_form
+# import requests, json
+# ##
+
 import models
 import os
 from flask_sslify import SSLify
 
 app = Flask(__name__)
 SSLify(app)
+Bootstrap(app)
+
+### some stuff for bootstrap
+
+app.config['BOOTSTRAP_USE_MINIFIED'] = True
+app.config['BOOTSTRAP_USE_CDN'] = True
+app.config['BOOTSTRAP_FONTAWESOME'] = True
+app.config['SECRET_KEY'] = 'devkey'
+
 
 app.secret_key = "this_is_a_secret_key_for_testing"
 if 'SECRET_KEY' in os.environ:
     app.secret_key = os.environ['SECRET_KEY']
+
+class ExampleForm(Form):
+    field1 = TextField('First Field', description='This is field one.')
+    field2 = TextField('Second Field', description='This is field two.',
+                       validators=[DataRequired()])
+    hidden_field = HiddenField('You cannot see this', description='Nope')
 
 def login_required(f):
     @wraps(f)
@@ -19,6 +51,16 @@ def login_required(f):
             return redirect(url_for('login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
+
+@app.route('/test_form', methods=('GET', 'POST',))
+def test_form():
+    form = ExampleForm()
+    if form.validate_on_submit():
+        return "PASSED"
+    return render_template('example.html', form=form)
+
+
+
 
 @app.route('/')
 def hello_world():
@@ -34,21 +76,37 @@ def signup():
     if request.method == "GET":
         return render_template("signup.html")
     if request.method == "POST":
-        u = models.User()
-        u.email = request.form['email']
-        u.name = request.form['name']
-        u.password = request.form['password']
-        u.cost = request.form['submit_cost']
-        u.frequency = request.form['submit_frequency']
-        u.thingy = request.form['submit_thingy']
-        models.db.session.add(u)
-        models.db.session.commit()
+        form = models.SignupForm(request)
+        if form.validate():
 
-        # FIXME: can we redirect to login instead of doing it again?
-        myuser = models.User.query.filter_by(email=u.email).first()
-        session['user'] = myuser
-        flash("Thanks for signing up!", 'success')
-        return redirect(url_for('user'))
+            # u = models.User()
+            # u.email = request.form['email']
+            # u.name = request.form['name']
+            # u.password = request.form['password']
+            # u.cost = request.form['submit_cost']
+            # u.frequency = request.form['submit_frequency']
+            # u.thingy = request.form['submit_thingy']
+            # models.db.session.add(u)
+            # models.db.session.commit()
+            #
+            # # FIXME: can we redirect to login instead of doing it again?
+            # myuser = models.User.query.filter_by(email=u.email).first()
+            # session['user'] = myuser
+            # flash("Thanks for signing up!", 'success')
+            # return redirect(url_for('user'))
+            u = models.User()
+            u.email = form.email.data
+            u.name = form.name.data
+            u.password = form.password.data
+            u.cost = "100$"
+            u.frequency = "10"
+            u.thingy = "Gym membership"
+            models.db.session.add(u)
+            models.db.session.commit()
+            myuser = models.User.query.filter_by(email=u.email).first()
+            session['user'] = myuser
+            flash("Thanks for signing up!", 'success')
+            return redirect(url_for('user'))
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
