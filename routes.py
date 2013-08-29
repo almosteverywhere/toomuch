9215e1923d2d90359a28d3af41500b9deb95f9d0
Julie Lavoie <almosteverywhere@gmail.com>
2013-08-29 12:36:12 -0400

    port

--- routes.py
+++ routes.py
@@ -233,5 +233,7 @@ def update_pairing(id, pair_id):
         return render_template("pairing.html", pairing=p)
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.debug = True
+    # app.run()
