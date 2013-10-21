0e6277331f891c4fed93e29fd8b8e775e1cf0158
Julie Lavoie <almosteverywhere@gmail.com>
2013-10-21 12:51:41 -0400

    Cleaned up things a bit.

--- models.py
+++ models.py
@@ -1,37 +1,10 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from routes import Flask
-# hmm does this go here?
-# from flask_wtf import Form
-# from wtforms import TextField, DataRequired
 from wtforms import Form, TextField, validators
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
-# # a boolean is it done
-# # possibly associated with a given HS peeps if we can get into the HS auth
-# class Pairing(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     done = db.Column(db.Boolean, default=False)
-#     person = db.Column(db.String(80))
-#     week_id = db.Column(db.Integer,db.ForeignKey('week.id'))
-#
-#     def __repr__(self):
-#         return str(self.id)
-
-# # represents the week of hacker school
-# # have some function that maps the current date
-# # to which week it is
-# # need to change this to weekly_goal
-# # hardcode helper function somehow.
-# class Week(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     number = db.Column(db.Integer)
-#     pairings = db.relationship('Pairing', backref='week', lazy='dynamic')
-#     user_email = db.Column(db.Integer,db.ForeignKey('user.email'))
-#
-#     def __repr__(self):
-#         return "Hacker School Week #" + str(self.number)
 
 class User(db.Model):
     name = db.Column(db.String(80))
@@ -44,7 +17,3 @@ class User(db.Model):
 
     def __repr__(self):
         return self.email
-# hmm does this go here?
-#
-# class MyForm(Form):
-#     name = TextField(name, validators=[DataRequired()])
--- routes.py
+++ routes.py
@@ -3,7 +3,6 @@
 # print sys.modules
 from flask import Flask, render_template, flash, jsonify, request, redirect, session, url_for, g
 from functools import wraps
-# import requests, json
 
 from flask_wtf import Form
 from wtforms import TextField, HiddenField, validators
@@ -16,11 +15,10 @@ import os
 from flask_sslify import SSLify
 
 app = Flask(__name__)
-SSLify(app)
+# SSLify(app)
 Bootstrap(app)
 
 ### some stuff for bootstrap
-
 app.config['BOOTSTRAP_USE_MINIFIED'] = True
 app.config['BOOTSTRAP_USE_CDN'] = True
 app.config['BOOTSTRAP_FONTAWESOME'] = True
@@ -31,10 +29,9 @@ if 'SECRET_KEY' in os.environ:
     app.secret_key = os.environ['SECRET_KEY']
 
 class SignupForm(Form):
-
-    name = TextField('Name', description='Name.', validators=[DataRequired()])
-    email = TextField('Email', description='Email.', validators=[DataRequired()])
-    password = TextField('Password', description='Password.',
+    name = TextField('Name',validators=[DataRequired()])
+    email = TextField('Email', validators=[DataRequired()])
+    password = TextField('Password',
                        validators=[DataRequired()])
     submit_thingy = HiddenField('hidden_field')
     submit_cost = HiddenField('submit_cost')
@@ -48,14 +45,6 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-# @app.route('/signup', methods=['GET', 'POST'])
-# def test_form():
-#     form = SignupForm()
-#     if form.validate_on_submit():
-#         return "PASSED"
-#     return render_template('signup.html', form=form)
-#
-
 @app.route('/')
 def hello_world():
     return redirect('/first')
@@ -67,6 +56,7 @@ def first():
 
 @app.route('/signup', methods=["POST", "GET"])
 def signup():
+    print "IN SIGNUP"
     form = SignupForm()
     if request.method == "GET":
         return render_template("signup.html", form=form)
@@ -154,8 +144,6 @@ def update():
     # TODO verify is number
     times = int(myuser.frequency);
     new_frequency = times + add_times;
-    # should probs be an int to start
-    # return str(new_frequency);
     myuser.frequency = new_frequency;
     models.db.session.add(myuser);
     models.db.session.commit();
@@ -184,53 +172,6 @@ def update():
 #     return new_frequency
 
 
-
-
-## show a given week
-## then show with its pairings
-@app.route('/week/<id>')
-@login_required
-def show_week(id):
-    w = models.Week.query.get(id)
-    # how to et all the tasks
-    mypairings = models.Pairing.query.filter(models.Pairing.week_id == w.id).all()
-    return render_template("week.html", week=w, pairings=mypairings)
-
-#FIXME need some code for not finding
-
-@app.route('/week/<id>/pairing/<pair_id>', methods=["GET", "POST"])
-@login_required
-def show_pairing(id, pair_id):
-    # w = models.Week.query.get(id)
-    # how to et all the tasks
-    # do we need the week id?
-    p = models.Pairing.query.get(pair_id)
-    # Worksheet.query??????
-    return render_template("pairing.html", pairing=p)
-
-@app.route('/week/<id>/pairing/<pair_id>/edit', methods=["GET", "POST"])
-@login_required
-def update_pairing(id, pair_id):
-    # should check permissions somehoe!
-    # get the form data
-    if request.method == "GET":
-        # does need to be by id as well?
-        # not sure we need all these queries, we mostly just need the week id
-        w = models.Week.query.get(id)
-        p = models.Pairing.query.get(pair_id)
-        return render_template("edit_pairing.html", week=w, pairing=p)
-
-    elif request.method == "POST":
-        p = models.Pairing.query.get(pair_id)
-        p.person = request.form['person']
-        done = request.form['done']
-        # # return "Done is " + done
-        if done == 'True':
-            p.done = True
-        else: p.done = False
-        models.db.session.add(p)
-        models.db.session.commit()
-        return render_template("pairing.html", pairing=p)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
