4b9168bdf4ae3e9ff8536167ccc76277d434c23b
Julie Lavoie <almosteverywhere@gmail.com>
2013-10-26 12:58:17 -0400

    Fix

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from routes import Flask
 from wtforms import Form, TextField, validators
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
